7a2404137af39cbfb8318a2d0701ab1eb1cd4d64
ospFPS <119087612+ospFPS@users.noreply.github.com>
2025-07-08 03:25:01 +0000

    test ex00.c

--- ex00.c
+++ ex00.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+
+int number1 = 1;
+int number2 = 2;
+float number3 = 312.232;
+char abba = 'a';
+
 int main() {
-    printf("what sup ");
+    printf("what sup hello\n");
+    printf("How can help you :D\n");
+    printf("%d\n",number1+number2);
+    printf("%f\n",number3);
+    printf("%c\n",abba);
+    printf("%s","khai mao");
     return 0;
 }
