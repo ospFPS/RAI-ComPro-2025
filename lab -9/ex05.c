5bce960d8fda31ca9fa8d5e7a23821681622dbb0
ospFPS <119087612+ospFPS@users.noreply.github.com>
2025-09-16 04:46:48 +0000

    lab -9

--- lab -9/ex05.c
+++ lab -9/ex05.c
@@ -1,26 +1,37 @@
 #include <stdio.h>
 
-int main()
+void checkEvenOdd(int num,int*even_sum,int*odd_sum) 
+{
+    if (num%2==0)
+        *even_sum += num;
+    else
+        *odd_sum += num;
+}
+
+int getInput() 
+{
+    int num;
+    printf("Input: ");
+    scanf("%d", &num);
+    return num;
+}
+
+int main() 
 {
     int n,i;
-    int num,even_sum=0,odd_sum=0;
+    int num,even_sum = 0,odd_sum = 0;
 
     printf("N: ");
     scanf("%d", &n);
 
-    for (i = 0; i < n; i++)
+    for (i=0;i<n;i++) 
     {
-        printf("Input: ");
-        scanf("%d", &num);
-
-        if (num%2==0)
-            even_sum += num;
-        else
-            odd_sum += num;
+        num = getInput();
+        checkEvenOdd(num,&even_sum,&odd_sum);
     }
 
     printf("Output: Sum of even number: %d\n", even_sum);
     printf("        Sum of odd number: %d\n", odd_sum);
 
     return 0;
-}
+}S
